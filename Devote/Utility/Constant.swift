c3b7afca9513e1978c4e5f3f474347834beba78e
Nerijus Šurkys <46633513+surkeela@users.noreply.github.com>
2022-03-30 13:24:10 +0300

    WP
    core data setup

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Nerijus Surkys on 2022-03-30.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
